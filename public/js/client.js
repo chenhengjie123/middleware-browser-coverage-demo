8117a1c4cfd38dbfa88cbd331f1eca5e8a70c193
chenhengjie <chenhengjie123@gmail.com>
2017-06-02 15:45:49 +0800

    browser 端添加定时上传覆盖率数据

--- public/js/client.js
+++ public/js/client.js
@@ -6,3 +6,17 @@ $(function () {
         return link;
     });
 });
+
+// post window.__coverage__ to server every 2 seconds
+setInterval(function() {
+    $.ajax({
+        url: "/coverage/client",
+        type: "POST",
+        data: JSON.stringify(window.__coverage__),
+        contentType: "application/json; charset=utf-8",
+        dataType:"json",
+        success: function(data) {
+            console.log("success!");
+        }
+    });
+}, 2000);
